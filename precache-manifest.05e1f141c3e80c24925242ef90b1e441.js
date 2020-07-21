7a127350b2a99d192846c4b268525d57203ee283
192495474 <pramod.gawali27@gmail.com>
2020-07-21 18:33:42 +0530

    Updates

---
+++ precache-manifest.05e1f141c3e80c24925242ef90b1e441.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41dd68e37f827d3fda1f242e6be3c09b",
+    "url": "/farmermaster/index.html"
+  },
+  {
+    "revision": "24c7549915411fe93e92",
+    "url": "/farmermaster/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a4b7e7e446cbf26aa5e",
+    "url": "/farmermaster/static/js/2.8daf1556.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/farmermaster/static/js/2.8daf1556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24c7549915411fe93e92",
+    "url": "/farmermaster/static/js/main.75205564.chunk.js"
+  },
+  {
+    "revision": "452e79e09f201ff39e23",
+    "url": "/farmermaster/static/js/runtime-main.2795a239.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/farmermaster/static/media/logo.5d5d9eef.svg"
+  }
+]);
